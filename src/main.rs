9e63fae4aa93e99f523d49ee8aba9b440d78eddf
Christian Louise B. Aranas <aranaschristianlouise@gmail.com>
2024-07-11 08:36:38 +0800

    Implement basic routing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::fs::{FileServer, relative};
+
+#[macro_use] extern crate rocket;
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/online-shopping-solidjs", FileServer::from(relative!("dist")))
 }
+
