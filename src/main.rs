693f7944b1c415875385cf051833f4ee4ae8cd92
Christian Louise B. Aranas <aranaschristianlouise@gmail.com>
2024-07-13 22:59:24 +0800

    Add actual routes instead of FileServer mount

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
-use rocket::fs::{FileServer, relative};
-
 #[macro_use] extern crate rocket;
+
+use rocket::fs::NamedFile;
+use std::path::{PathBuf, Path};
+
+#[get("/assets/<file..>")]
+async fn shop_solidjs(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("online-shopping-solidjs/assets/").join(file)).await.ok()
+}
+
+#[get("/<_..>", rank = 2)]
+async fn files() -> Option<NamedFile> {
+    NamedFile::open(Path::new("online-shopping-solidjs/index.html")).await.ok()
+}
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/online-shopping-solidjs", FileServer::from("/home/chrsrns-projects-server/online-shopping-solidjs"))
+    rocket::build().mount("/online-shopping-solidjs", routes![files, shop_solidjs])
 }
 
