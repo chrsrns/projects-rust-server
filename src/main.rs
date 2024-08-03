b3a77e5348c971a460895bf7af731bf438046c1a
Christian Louise B. Aranas <aranaschristianlouise@gmail.com>
2024-08-03 22:19:52 +0800

    WIP Updated rocket launch function

--- src/main.rs
+++ src/main.rs
@@ -23,35 +23,9 @@ async fn files() -> Option<NamedFile> {
 }
 
 #[launch]
-fn rocket() -> _ {
-    let client_connection = Client::connect(
-        "postgresql://postgres:postgres@localhost:5432/postgres",
-        NoTls,
-    );
-    if client_connection.is_err() {
-        // Just print an error message. The attempt to unwrap will terminate the program.
-        println!("Failed to connect to PostgreSQL database. Termination imminent...");
-    }
-    let mut client = client_connection.unwrap();
+async fn rocket() -> _ {
 
-    // TODO: Consider moving the function defs behind an encapsulated API
-    match db::init_database(&mut client) {
-        Ok(_) => println!("Successfully initialized database"),
-        Err(error) => {
-            println!("Failed to initialize database. Aborting... ");
-            let unwrapped = error.code().unwrap().code();
-            println!("Error reason: {}", unwrapped);
-        }
-    };
-    match User::get_all_users(&mut client) {
-        Ok(results) => {
-            for result in results {
-                println!("Username: {}", result.username);
-            }
-        }
-        Err(_) => {
-            println!("Error when getting all users");
-        }
-    }
-    rocket::build().mount("/online-shopping-solidjs", routes![files, shop_solidjs])
+    rocket::build()
+        .attach(Db::init())
+        .mount("/", routes![files, shop_solidjs, users])
 }
