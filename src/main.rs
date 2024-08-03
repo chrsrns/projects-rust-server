94dd3b73b11e23dcec9c2721dcc8b33f28ca8c50
Christian Louise B. Aranas <aranaschristianlouise@gmail.com>
2024-08-03 22:23:09 +0800

    WIP Added further changes
    This adds the remaining changes for porting.

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,33 @@
 extern crate rocket;
 
 use db::user::User;
-use postgres::{Client, NoTls};
-use rocket::fs::NamedFile;
+use futures::stream::TryStreamExt;
+use rocket::serde::json::Json;
+use rocket::{fairing, Build};
+use rocket::{fs::NamedFile, Rocket};
+use rocket_db_pools::{Connection, Database};
 use std::path::{Path, PathBuf};
 
 mod db;
+
+#[derive(Database)]
+#[database("sqlx")]
+struct Db(sqlx::PgPool);
+
+type Result<T, E = rocket::response::Debug<sqlx::Error>> = std::result::Result<T, E>;
+
+pub async fn init_database(rocket: Rocket<Build>) -> fairing::Result {
+    match Db::fetch(&rocket) {
+        Some(db) => match sqlx::migrate!("src/db/migrations").run(&**db).await {
+            Ok(_) => Ok(rocket),
+            Err(e) => {
+                error!("Failed to initialize SQLx database: {}", e);
+                Err(rocket)
+            }
+        },
+        None => Err(rocket),
+    }
+}
 
 #[get("/online-shopping-solidjs/assets/<file..>")]
 async fn shop_solidjs(file: PathBuf) -> Option<NamedFile> {
