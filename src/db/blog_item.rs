18a0c4cca2d57d034880f7b6f36f654d88292d13
Christian Louise B. Aranas <aranaschristianlouise@gmail.com>
2024-08-06 22:31:40 +0800

    Updated blog item structs

--- src/db/blog_item.rs
+++ src/db/blog_item.rs
@@ -1,12 +1,16 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, sqlx::Type)]
+#[sqlx(type_name = "content_type", rename_all = "lowercase")]
 pub enum ContentType {
+    BigHeader,
     Header,
+    SmallHeader,
     Body,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, sqlx::FromRow)]
+#[sqlx(type_name = "blog_item")]
 pub struct BlogItem {
     #[serde(skip_deserializing, skip_serializing_if = "Option::is_none")]
     pub id: Option<i32>,
@@ -15,9 +19,11 @@ pub struct BlogItem {
     pub content: Vec<Content>,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, sqlx::FromRow)]
+#[sqlx(type_name = "content")]
 pub struct Content {
     #[serde(skip_deserializing, skip_serializing_if = "Option::is_none")]
     pub id: Option<i32>,
     pub ctype: ContentType,
+    pub content: String,
 }
