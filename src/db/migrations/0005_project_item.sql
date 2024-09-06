3c940d2f31692e1da4429e53c327a62877ebe7c6
Christian Louise B. Aranas <aranaschristianlouise@gmail.com>
2024-09-06 21:47:48 +0800

    Added sql code and struct for tags and tag relation

--- src/db/migrations/0005_project_item.sql
+++ src/db/migrations/0005_project_item.sql
@@ -12,3 +12,20 @@ CREATE TABLE IF NOT EXISTS project_desc_item (
         project_id
     ) REFERENCES project_item (id)
 );
+
+CREATE TABLE IF NOT EXISTS tag (
+    id SERIAL PRIMARY KEY,
+    text VARCHAR UNIQUE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS project_tech_tag (
+    id SERIAL PRIMARY KEY,
+    project_id INT NOT NULL,
+    tag_id INT NOT NULL,
+    CONSTRAINT fk_project FOREIGN KEY (
+        project_id
+    ) REFERENCES project_item (id),
+    CONSTRAINT fk_tag FOREIGN KEY (
+        tag_id
+    ) REFERENCES tag (id)
+);
--- src/db/project_item.rs
+++ src/db/project_item.rs
@@ -18,3 +18,20 @@ pub struct DescItem {
     pub project_id: Option<i32>,
     pub content: String,
 }
+
+#[derive(Serialize, Deserialize, Clone, sqlx::FromRow)]
+#[sqlx(type_name = "tag")]
+pub struct Tag {
+    #[serde(skip_deserializing, skip_serializing_if = "Option::is_none")]
+    pub id: Option<i32>,
+    pub text: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, sqlx::FromRow)]
+#[sqlx(type_name = "project_tech_tag")]
+pub struct ProjectToTechTag {
+    #[serde(skip_deserializing, skip_serializing_if = "Option::is_none")]
+    pub id: Option<i32>,
+    pub project_id: i32,
+    pub tag_id: i32,
+}
