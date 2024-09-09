80f9de2ac7171783b2204f1605abc911b64e42cf
Christian Louise B. Aranas <aranaschristianlouise@gmail.com>
2024-09-09 20:54:02 +0800

    Implemented add functions to tag struct

--- src/db/tag.rs
+++ src/db/tag.rs
@@ -1,3 +1,5 @@
+use crate::Db;
+use rocket_db_pools::Connection;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone, sqlx::FromRow)]
@@ -15,4 +17,60 @@ pub struct ProjectToTechTag {
     pub id: Option<i32>,
     pub project_id: i32,
     pub tag_id: i32,
+}
+
+impl Tag {
+    pub async fn add(&self, mut db: Connection<Db>) -> Result<Tag, sqlx::Error> {
+        let result = sqlx::query_as!(
+            Tag,
+            "INSERT INTO tag (text) VALUES ($1) RETURNING id, text",
+            &self.text,
+        )
+        .fetch_one(&mut **db)
+        .await;
+
+        match result {
+            Ok(result) => {
+                println!("Successfully added new  {}", &self.text);
+
+                Ok(result)
+            }
+            Err(error) => {
+                println!("Error when creating new blog item with: [ {} ]", &self.text);
+                Err(error)
+            }
+        }
+    }
+}
+
+impl ProjectToTechTag {
+    pub async fn add(
+        mut db: Connection<Db>,
+        project_id: &i32,
+        tag_id: &i32,
+    ) -> Result<ProjectToTechTag, sqlx::Error> {
+        let result = sqlx::query_as!(
+            ProjectToTechTag,
+            "INSERT INTO project_tech_tag (project_id, tag_id) VALUES ($1, $2) RETURNING id, project_id, tag_id",
+&project_id, &tag_id
+            ,
+        )
+        .fetch_one(&mut **db)
+        .await;
+
+        match result {
+            Ok(result) => {
+                println!("Successfully added new  {}", result.id.unwrap_or(-1));
+
+                Ok(result)
+            }
+            Err(error) => {
+                println!(
+                    "Error when joining project {} and tag {}",
+                    &project_id, &tag_id
+                );
+                Err(error)
+            }
+        }
+    }
 }
